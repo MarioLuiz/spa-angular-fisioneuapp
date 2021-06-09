9f3cdb5588f3126db03a35aed6f5b148df9fb61c
MarioLuiz <marioluiz.arruda@gmail.com>
2021-06-08 21:05:02 -0400

    Criado componente de acesso e adicionado rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {ROUTES} from './app.routes'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcessoComponent } from './acesso/acesso.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcessoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { AcessoComponent } from "./acesso/acesso.component";
+
+export const ROUTES: Routes = [
+    {path:'', component: AcessoComponent}
+]
