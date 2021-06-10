0364b3feb886502af1f374d95fe31e3e35230b1b
MarioLuiz <marioluiz.arruda@gmail.com>
2021-06-09 21:05:08 -0400

    Adicionado componentes Login e Cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
 import { RouterModule } from '@angular/router';
+import { LoginComponent } from './acesso/login/login.component';
+import { CadastroComponent } from './acesso/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AcessoComponent
+    AcessoComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
