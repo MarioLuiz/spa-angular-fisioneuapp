084c2e5f046aee752b1dad31c4cb8116a629af79
MarioLuiz <marioluiz.arruda@gmail.com>
2021-08-30 22:12:10 -0400

    Adicionada rota para edição cadastro Fisioterapeuta.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,15 @@ import { Routes } from "@angular/router";
 import { AcessoComponent } from "./acesso/acesso.component";
 import { AutenticacaoGuardService } from "./autenticacao-guard.service";
 import { HomeComponent } from "./home/home.component";
+import { EdicaoCadastroFisioterapeutaComponent } from './edicao-cadastro-fisioterapeuta/edicao-cadastro-fisioterapeuta.component';
 
 export const ROUTES: Routes = [
-    {path:'', component: AcessoComponent},
-    {path:'fisio', component:HomeComponent, canActivate: [AutenticacaoGuardService]}
+    { path: '', component: AcessoComponent },
+    {
+        path: 'fisio', component: HomeComponent, canActivate: [AutenticacaoGuardService],
+        children: [
+            { path: '', component: HomeComponent, canActivate: [AutenticacaoGuardService] },
+            { path: 'atualizar-cadastro-fisioterapeuta', component: EdicaoCadastroFisioterapeutaComponent, canActivate: [AutenticacaoGuardService] },
+        ]
+    }
 ]
