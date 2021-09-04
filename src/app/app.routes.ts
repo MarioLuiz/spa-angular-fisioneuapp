f35a04a274f40d6c6aa7bfdba8d3fcae4a19f5a6
MarioLuiz <marioluiz.arruda@gmail.com>
2021-09-03 23:19:02 -0400

    Adicionada rota componente recuperar senha.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AcessoComponent } from "./acesso/acesso.component";
 import { AutenticacaoGuardService } from "./autenticacao-guard.service";
 import { HomeComponent } from "./home/home.component";
 import { EdicaoCadastroFisioterapeutaComponent } from './edicao-cadastro-fisioterapeuta/edicao-cadastro-fisioterapeuta.component';
+import { EdicaoSenhaFisioterapeutaComponent } from './edicao-senha-fisioterapeuta/edicao-senha-fisioterapeuta.component';
 
 export const ROUTES: Routes = [
     { path: '', component: AcessoComponent },
@@ -11,6 +12,7 @@ export const ROUTES: Routes = [
         children: [
             { path: '', component: HomeComponent, canActivate: [AutenticacaoGuardService] },
             { path: 'atualizar-cadastro-fisioterapeuta', component: EdicaoCadastroFisioterapeutaComponent, canActivate: [AutenticacaoGuardService] },
+            { path: 'atualizar-senha-fisioterapeuta', component: EdicaoSenhaFisioterapeutaComponent, canActivate: [AutenticacaoGuardService]}
         ]
     }
 ]
