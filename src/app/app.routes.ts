524b636c55f8739d1c6b0872b2abf773d57b05f4
MarioLuiz <marioluiz.arruda@gmail.com>
2021-09-24 20:42:49 -0400

    Adicionado rota para componente cadastro paciente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AutenticacaoGuardService } from "./autenticacao-guard.service";
 import { HomeComponent } from "./home/home.component";
 import { EdicaoCadastroFisioterapeutaComponent } from './edicao-cadastro-fisioterapeuta/edicao-cadastro-fisioterapeuta.component';
 import { EdicaoSenhaFisioterapeutaComponent } from './edicao-senha-fisioterapeuta/edicao-senha-fisioterapeuta.component';
+import { CadastroPacienteComponent } from "./cadastro-paciente/cadastro-paciente.component";
 
 export const ROUTES: Routes = [
     { path: '', component: AcessoComponent },
@@ -12,7 +13,8 @@ export const ROUTES: Routes = [
         children: [
             { path: '', component: HomeComponent, canActivate: [AutenticacaoGuardService] },
             { path: 'atualizar-cadastro-fisioterapeuta', component: EdicaoCadastroFisioterapeutaComponent, canActivate: [AutenticacaoGuardService] },
-            { path: 'atualizar-senha-fisioterapeuta', component: EdicaoSenhaFisioterapeutaComponent, canActivate: [AutenticacaoGuardService]}
+            { path: 'atualizar-senha-fisioterapeuta', component: EdicaoSenhaFisioterapeutaComponent, canActivate: [AutenticacaoGuardService]},
+            { path: 'cadastrar-paciente', component: CadastroPacienteComponent, canActivate: [AutenticacaoGuardService] },
         ]
     }
 ]
