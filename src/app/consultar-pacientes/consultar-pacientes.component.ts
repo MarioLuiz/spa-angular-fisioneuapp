b53d56f265be3a5c0072bc7ee1bcf8339f8f725c
MarioLuiz <marioluiz.arruda@gmail.com>
2021-10-12 17:52:27 -0400

    Adicionada consulta pacientes por Paginação e termo

--- src/app/consultar-pacientes/consultar-pacientes.component.ts
+++ src/app/consultar-pacientes/consultar-pacientes.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Observable, of, Subject, throwError } from 'rxjs';
+import { Paginacao } from 'src/assets/models/paginacao.model';
+import { PacienteService } from '../paciente.service';
+import { debounceTime, distinctUntilChanged, switchMap, catchError } from 'rxjs/operators'
 
 @Component({
   selector: 'fisio-consultar-pacientes',
   templateUrl: './consultar-pacientes.component.html',
   styleUrls: ['./consultar-pacientes.component.scss']
 })
-export class ConsultarPacientesComponent implements OnInit {
+export class ConsultarPacientesComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  palavraDaPesquisa: string = ''
+  mensagensErroConsulta: string[] = []
+  paginacao: Paginacao = new Paginacao();
+  //pacientes: Observable<any> | undefined
+  //private subjectPesquisa: Subject<string> = new Subject<string>()
+
+  constructor(
+    private pacienteService: PacienteService
+  ) { }
 
   ngOnInit(): void {
+    /*
+    this.pacientes = this.subjectPesquisa.pipe( //retorno Pacientes[]
+      debounceTime(1000), // Executa a ação do switchMap após 1 segundo
+      distinctUntilChanged(), // previne pesquisa de termo identico ao termo anteriormente pesquisado
+      switchMap((termo: string) => {
+        
+        if (termo.trim() === '') {
+          // retorna um observable de array de 'pacientes' vazio
+          return of<any[]>([])
+        }
+        
+
+        return this.pacienteService.consultarPacientesPaginado(this.paginacao, termo)
+      }),
+      catchError((erro: any, observable: Observable<any[]>) => {
+        console.log('Erro ao Pesquisar oferta: ', erro)
+        return observable
+      })
+    )
+    */
+  }
+
+  ngAfterViewInit() {
+    // this.subjectPesquisa.next(this.termoDaPesquisa)
+    // console.log('Pacientes: ', this.pacientes)
+    this.pesquisa();
+  }
+
+  pesquisa() {
+    console.log('Termo Pesquisado: ', this.palavraDaPesquisa)
+    this.pacienteService.consultarPacientesPaginado(this.paginacao, this.palavraDaPesquisa)
+      .pipe(
+        catchError(err => {
+          return throwError(err);
+        })
+      )
+      .subscribe(
+        resposta => {
+          console.log('Pacientes', resposta)
+        },
+        (err: any) => {
+          console.log('Erro ao salvar Paciente: ', err)
+          this.mensagensErroConsulta = []
+          err.error.errors.forEach((mensagemErro: any) => {
+            this.mensagensErroConsulta.push(mensagemErro.fieldName + ' : ' + mensagemErro.message);
+          });
+          //this.estadoAnimacaoPainelCadastro = 'criado'
+        }
+      )
   }
 
 }
--- src/app/paciente.service.ts
+++ src/app/paciente.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { map, retry } from "rxjs/operators";
 import { Paciente } from "src/assets/models/paciente.model";
+import { Paginacao } from "src/assets/models/paginacao.model";
 import { URL_API } from './app.api';
 
 @Injectable()
@@ -30,6 +31,31 @@ export class PacienteService {
             observe: "response" as 'body'
         }
         return this.http.post(`${URL_API}/pacientes`, JSON.stringify(paciente), options).pipe(
+            map((resposta: any) => resposta),
+            retry(3)
+        )
+    }
+
+    public consultarPacientesPaginado(paginacao: Paginacao, termo: string): Observable<any> {
+        let headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': `${localStorage.getItem('idToken')}`
+        })
+
+        let params = new HttpParams()
+            .set("page", paginacao.page)
+            .set("linesPerPage", paginacao.linesPerPage)
+            .set("orderBy", paginacao.orderBy)
+            .set("direction", paginacao.direction)
+            .set("nome", termo)
+
+        let options = {
+            headers: headers,
+            observe: "response" as 'body',
+            params: params
+        }
+        
+        return this.http.get(`${URL_API}/pacientes/page/custom`, options).pipe(
             map((resposta: any) => resposta),
             retry(3)
         )
