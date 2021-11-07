9229092f067bf23f029aae05d9487b6add486610
MarioLuiz <marioluiz.arruda@gmail.com>
2021-11-07 13:46:57 -0400

    Adicionado paciente ao prontuarioModel

--- src/assets/models/prontuario.model.ts
+++ src/assets/models/prontuario.model.ts
@@ -1,3 +1,5 @@
+import { Paciente } from "./paciente.model"
+
 export class Prontuario {
     public id: string | undefined
     public pacienteId: string | undefined
@@ -6,6 +8,7 @@ export class Prontuario {
     public cif: string | undefined
     public observacao: string | undefined
     public dataCadastro: string | undefined
+    public paciente: Paciente | undefined
 
     constructor(pacienteId: string, numero: string, cid: string, cif: string, observacao: string) {
         this.pacienteId = pacienteId
